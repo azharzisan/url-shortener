253cbd1dc643a808bf6ece4c06e60fb2473cac17
Azhar Zisan <azharzisanxtr@gmail.com>
2026-05-02 13:12:12 +0100

    url data updating logic added and working

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 const {nanoid} = require("nanoid");
-const { json } = require("stream/consumers");
+const { ChildProcess } = require("child_process");
 
-const shortUrlExt = nanoid(10);
 
 app.use(express.json())
 
 app.post("/shorten", (req, res) => {
+    const shortUrlExt = nanoid(10);
     const {url} = req.body;
 
-    const UrlsData = {
+    const urlsData = {
         baseUrl : url,
         redirectingUrl : `http://locahost:3000/${shortUrlExt}`
     }
-    res.json(UrlsData)
-    fs.writeFileSync(path.join(__dirname, "data", "urls.json"), JSON.stringify(UrlsData))
+    const existingRaw = fs.readFileSync(path.join(__dirname, "data", "urls.json"), "utf-8")
+    const existingData = JSON.parse(existingRaw) ?? []
+
+    existingData.push(urlsData)
+    
+    fs.writeFileSync(path.join(__dirname, "data", "urls.json"), JSON.stringify(existingData, null, 2))
+    res.json(urlsData)
 })
 
 app.listen(3000, () => {
